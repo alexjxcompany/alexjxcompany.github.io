78cdcc50c0bd66bc17b72019458268909d5ef6d4
alexjxcompany <43199927+alexjxcompany@users.noreply.github.com>
2018-09-12 18:31:42 +0300

    Update check_user.php

--- check_user.php
+++ check_user.php
@@ -26,7 +26,7 @@ $myrow1 = mysqli_fetch_array($result);
 $result = mysqli_query($db, "SELECT id FROM users WHERE pass='$password'");
 $myrow2 = mysqli_fetch_array($result);
 
-if ($myrow1 != $myrow2)
+if (($myrow1 != $myrow2) || (empty($myrow1)) || (empty($myrow2)) )
 {exit ("Wrong login or password!");
 } else header("Refresh: 1;  url=chat.php?login=$login");
 
